12022236504d57aa66e5e7469d1697aae88f6bc9
David Boreham <david@bozemanpass.com>
2022-08-13 22:30:12 -0600

    More compose file updates

--- container-build/vulcanize-go-ethereum-foundry/build.sh
+++ container-build/vulcanize-go-ethereum-foundry/build.sh
@@ -4,4 +4,4 @@
 # See: https://stackoverflow.com/a/246128/1701505
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker build -t vulcanize/go-ethereum-foundry:local ${SCRIPT_DIR}
+docker build -t vulcanize/go-ethereum-foundry:local --build-arg GENESIS_FILE_PATH=genesis-automine.json ${SCRIPT_DIR}
--- container-build/vulcanize-test-contract/build.sh
+++ container-build/vulcanize-test-contract/build.sh
@@ -1,2 +1,2 @@
 # Build vulcanize/test-contract
-docker build -t vulcanize/test-contract:local ${VULCANIZE_REPO_BASE_DIR}/ipld-eth-db-validator/test/contract
+docker build -t vulcanize/test-contract:local --build-arg ETH_ADDR=http://go-ethereum:8545 ${VULCANIZE_REPO_BASE_DIR}/ipld-eth-db-validator/test/contract

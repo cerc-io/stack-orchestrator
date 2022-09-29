7a32c701521e1237ae987d3f15a702cb40a531b7
David Boreham <david@bozemanpass.com>
2022-09-28 20:32:58 -0600

    Fixes for laconic-sdk

--- container-build/cerc-laconic-client/build.sh
+++ container-build/cerc-laconic-client/build.sh
@@ -4,4 +4,5 @@
 # See: https://stackoverflow.com/a/246128/1701505
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker build -t cerc/laconic-client:local -f ${SCRIPT_DIR}/Dockerfile --build-arg NPM_AUTH_TOKEN=$(NPM_AUTH_TOKEN) ${CERC_REPO_BASE_DIR}/laconic-client
+# TODO: change the repo name to laconic-client, if it becomes re-named (laconic-client currently is just the TS protocol client library, not the CLI tool)
+docker build -t cerc/laconic-client:local -f ${SCRIPT_DIR}/Dockerfile --build-arg NPM_AUTH_TOKEN=$(NPM_AUTH_TOKEN) ${CERC_REPO_BASE_DIR}/laconic-sdk

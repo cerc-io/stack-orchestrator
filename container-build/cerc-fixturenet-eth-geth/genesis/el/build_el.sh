776bce8ec6cc735730f84e196d675e577a28acb9
Thomas E Lackey <telackey@bozemanpass.com>
2022-10-14 15:48:18 -0500

    Auto-generate accounts.csv

--- container-build/cerc-fixturenet-eth-geth/genesis/el/build_el.sh
+++ container-build/cerc-fixturenet-eth-geth/genesis/el/build_el.sh
@@ -7,6 +7,6 @@ mkdir -p ../build/el
 
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 envsubst < el-config.yaml > $tmp_dir/genesis-config.yaml
+
 python3 /apps/el-gen/genesis_geth.py $tmp_dir/genesis-config.yaml   > ../build/el/geth.json
-python3 /apps/el-gen/genesis_chainspec.py $tmp_dir/genesis-config.yaml > ../build/el/chainspec.json
-python3 /apps/el-gen/genesis_besu.py $tmp_dir/genesis-config.yaml > ../build/el/besu.json
+python3 ../accounts/mnemonic_to_csv.py $tmp_dir/genesis-config.yaml > ../build/el/accounts.csv

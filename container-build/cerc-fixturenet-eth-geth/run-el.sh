2bd5c729dceb6535801f5feca643ccdbbfef1dd8
Thomas E Lackey <telackey@bozemanpass.com>
2022-10-14 13:48:54 -0500

    Use config/

--- container-build/cerc-fixturenet-eth-geth/run-el.sh
+++ container-build/cerc-fixturenet-eth-geth/run-el.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+ETHERBASE=`cat /opt/testnet/accounts/accounts.csv | head -1 | cut -d',' -f2`
+NETWORK_ID=`cat /opt/testnet/el/el-config.yaml | grep 'chain_id' | awk '{ print $2 }'`
+
 if [ "true" == "$RUN_BOOTNODE" ]; then 
     geth \
       --nodekeyhex="b0ac22adcad37213c7c565810a50f1772291e7b0ce53fb73e7ec2a3c75bc13b5" \
       --nodiscover \
       --ipcdisable \
       --networkid=${NETWORK_ID} \
-      --netrestrict="172.16.254.0/28"  2>&1 | tee /var/log/geth_bootnode.log
+      --netrestrict="${NETRESTRICT}"  2>&1 | tee /var/log/geth_bootnode.log
 else
     cd /opt/testnet/accounts
     ./import_keys.sh
@@ -25,7 +28,7 @@ else
       --authrpc.vhosts="*" \
       --authrpc.jwtsecret="/opt/testnet/build/el/jwtsecret" \
       --networkid=${NETWORK_ID} \
-      --netrestrict="172.16.254.0/28" \
+      --netrestrict="${NETRESTRICT}" \
       --syncmode=full \
       --mine \
       --miner.threads=1 \

16c5c45db8829ccb06c29b9241619b7d1ad0552a
David Boreham <david@bozemanpass.com>
2022-11-04 06:58:16 -0600

    Add place to figure out test script steps

---
+++ experimental/laconicd-test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Start fixturenet-laconicd
+# Wait for chain to start
+# Run command to extract key
+# docker exec -it 383cdc5afeb1 laconicd keys export mykey --unarmored-hex --unsafe
+# WARNING: The private key will be exported as an unarmored hexadecimal string. USE AT YOUR OWN RISK. Continue? [y/N]: y
+# ef564f8af713f62112cb6588bc2a33ece873f9a4336821fffeda9477667b3b8f
+# Configure key in env var
+# Find external ports
+# Configure env vars for ports
+# Start laconicd-test container in separate cluster?
+# Tell it to run tests
+# Grab results from log

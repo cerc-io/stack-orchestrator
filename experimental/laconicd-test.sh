3cd27e49b58cbfb7e4dd0eedc821dbe12422811e
David Boreham <david@bozemanpass.com>
2022-11-04 07:01:06 -0600

    Add more notes

--- experimental/laconicd-test.sh
+++ experimental/laconicd-test.sh
@@ -7,7 +7,10 @@
 # WARNING: The private key will be exported as an unarmored hexadecimal string. USE AT YOUR OWN RISK. Continue? [y/N]: y
 # ef564f8af713f62112cb6588bc2a33ece873f9a4336821fffeda9477667b3b8f
 # Configure key in env var
+# export PRIVATE_KEY=ef564f8af713f62112cb6588bc2a33ece873f9a4336821fffeda9477667b3b8f
 # Find external ports
+# restEndpoint: process.env.LACONICD_REST_ENDPOINT || 'http://localhost:1317',
+#     gqlEndpoint: process.env.LACONICD_GQL_ENDPOINT || 'http://localhost:9473/api'
 # Configure env vars for ports
 # Start laconicd-test container in separate cluster?
 # Tell it to run tests

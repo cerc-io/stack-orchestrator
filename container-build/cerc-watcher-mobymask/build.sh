acf3877caa617e233f6c0ffb5537fb0bfe6016a1
David Boreham <david@bozemanpas.com>
2022-11-06 17:17:48 -0700

    Working container build

--- container-build/cerc-watcher-mobymask/build.sh
+++ container-build/cerc-watcher-mobymask/build.sh
@@ -4,4 +4,5 @@
 # See: https://stackoverflow.com/a/246128/1701505
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker build -t cerc/watcher-mobymask:local -f ${SCRIPT_DIR}/Dockerfile ${CERC_REPO_BASE_DIR}/mobymask-watcher
+# TODO: add a mechanism to pass two repos into a container rather than the parent directory as below
+docker build -t cerc/watcher-mobymask:local -f ${SCRIPT_DIR}/Dockerfile ${CERC_REPO_BASE_DIR}

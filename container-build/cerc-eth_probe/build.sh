bd7b723eef30e3ed1947e188aa3ba5c22a4e145b
Thomas E Lackey <telackey@bozemanpass.com>
2022-12-05 15:05:18 -0600

    eth-probe

--- container-build/cerc-eth_probe/build.sh
+++ container-build/cerc-eth_probe/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Build cerc/eth_probe
-docker build -t cerc/eth_probe:local ${CERC_REPO_BASE_DIR}/eth_probe
+# Build cerc/eth-probe
+docker build -t cerc/eth-probe:local ${CERC_REPO_BASE_DIR}/eth_probe

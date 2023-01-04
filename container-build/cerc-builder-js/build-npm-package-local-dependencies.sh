b10b6c19f912d83ff53d870ddbd719a4b1b9eaa2
David Boreham <david@bozemanpass.com>
2023-01-03 21:14:35 -0700

    Use variable for local package scope

--- container-build/cerc-builder-js/build-npm-package-local-dependencies.sh
+++ container-build/cerc-builder-js/build-npm-package-local-dependencies.sh
@@ -16,10 +16,10 @@ package_publish_version=$2
 # TODO: make this a paramater and allow a list of scopes
 npm_scope_for_local="@cerc-io"
 # We need to configure the local registry 
-npm config set @cerc-io:registry ${local_npm_registry_url}
+npm config set ${npm_scope_for_local}:registry ${local_npm_registry_url}
 npm config set -- ${local_npm_registry_url}:_authToken ${NPM_AUTH_TOKEN}
 # Find the set of dependencies from the specified scope
-mapfile -t dependencies_from_scope < <(cat package.json | jq -r '.dependencies | with_entries(if (.key|test("^@cerc-io/.*$")) then ( {key: .key, value: .value } ) else empty end ) | keys[]')
+mapfile -t dependencies_from_scope < <(cat package.json | jq -r '.dependencies | with_entries(if (.key|test("^'${npm_scope_for_local}'/.*$")) then ( {key: .key, value: .value } ) else empty end ) | keys[]')
 echo "Fixing up dependencies"
 for package in "${dependencies_from_scope[@]}"
 do

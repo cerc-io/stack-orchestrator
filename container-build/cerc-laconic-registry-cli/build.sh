6e67a59251be24f97224206025573421af5392c3
David Boreham <david@bozemanpass.com>
2023-01-06 13:44:52 -0700

    Initial implementation

--- container-build/cerc-laconic-registry-cli/build.sh
+++ container-build/cerc-laconic-registry-cli/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-# Build cerc/laconic-cns-cli
+# Build cerc/laconic-registry-cli
 
 # See: https://stackoverflow.com/a/246128/1701505
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker build -t cerc/laconic-cns-cli:local -f ${SCRIPT_DIR}/Dockerfile --build-arg NPM_AUTH_TOKEN=$(NPM_AUTH_TOKEN) ${CERC_REPO_BASE_DIR}/laconic-cns-cli
+docker build -t cerc/laconic-registry-cli:local -f ${SCRIPT_DIR}/Dockerfile \
+  --add-host host.docker.internal:host-gateway \
+  --build-arg CERC_LOCAL_NPM_TOKEN --build-arg CERC_LOCAL_NPM_URL ${SCRIPT_DIR}

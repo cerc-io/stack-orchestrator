d315941f210e6379eb377318224c1e3fc9614123
David Boreham <david@bozemanpass.com>
2023-01-10 14:23:59 -0700

    Version command implemented

--- scripts/update_version_file.sh
+++ scripts/update_version_file.sh
@@ -1,3 +1,7 @@
 version_file_name=./app/data/version.txt
 echo "# This file should be re-generated running: scripts/update-version-file.sh script" > $version_file_name
-echo $( git describe --tags --abbrev=0 ; git rev-parse --short HEAD ) >> $version_file_name
+tag_string=$( git describe --tags --abbrev=0 )
+commit_string=$( git rev-parse --short HEAD )
+version_string=${tag_string}-${commit_string}
+echo ${version_string} >> $version_file_name
+echo ${version_string}

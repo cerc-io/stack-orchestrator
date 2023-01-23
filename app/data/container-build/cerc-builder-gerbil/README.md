8843b894e22044a0e3d16b57abbad6f4fa8f8a42
David Boreham <david@bozemanpass.com>
2023-01-22 21:50:50 -0700

    Add gerbil builder container

---
+++ app/data/container-build/cerc-builder-gerbil/README.md
@@ -0,0 +1,6 @@
+## Gerbil Scheme Builder
+
+This container is designed to be used as a simple "build runner" environment for building and running Scheme projects
+using Gerbil and gerbil-ethereum.
+
+
---
+++ app/data/container-build/cerc-builder-gerbil/install-dependencies.sh
@@ -0,0 +1,15 @@
+DEPS=(github.com/fare/gerbil-utils
+      github.com/fare/gerbil-poo
+      github.com/fare/gerbil-crypto
+      github.com/fare/gerbil-persist
+      github.com/fare/gerbil-ethereum
+      github.com/drewc/gerbil-swank
+      github.com/drewc/drewc-r7rs-swank
+      github.com/drewc/smug-gerbil
+      github.com/drewc/ftw
+      github.com/vyzo/gerbil-libp2p
+      ) ;
+for i in ${DEPS[@]} ; do
+  gxpkg install $i &&
+  gxpkg build $i
+done

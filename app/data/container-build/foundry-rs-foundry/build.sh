14e57abfbfa2d137f910d46080569773ebfa8456
David Boreham <david@bozemanpass.com>
2023-01-23 17:07:23 -0700

    Revert debugging code

--- app/data/container-build/foundry-rs-foundry/build.sh
+++ app/data/container-build/foundry-rs-foundry/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Build foundry-rs/foundry
 # HACK below : TARGETARCH needs to be derived from the host environment
-docker build -t foundry-rs/foundry:local --build-arg TARGETARCH=arm64 ${CERC_REPO_BASE_DIR}/foundry
+docker build -t foundry-rs/foundry:local ${CERC_REPO_BASE_DIR}/foundry

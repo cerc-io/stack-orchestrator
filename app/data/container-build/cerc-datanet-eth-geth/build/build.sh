22077dedc8979816298a242fae28e256125a0e51
Thomas E Lackey <telackey@bozemanpass.com>
2023-01-28 00:51:11 -0600

    Remove generated files.
    Former-commit-id: d0c506af881fee53f174f6d8380c1e2b36cdc3d6

--- app/data/container-build/cerc-datanet-eth-geth/build/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Build cerc/fixturenet-eth-geth
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-docker build -t cerc/fixturenet-eth-geth:local -f ${SCRIPT_DIR}/Dockerfile $SCRIPT_DIR

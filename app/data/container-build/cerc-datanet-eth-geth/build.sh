c0455e3e0b172584db9bd78167023d45a7d59d08
Thomas E Lackey <telackey@bozemanpass.com>
2023-01-30 11:20:23 -0600

    Simplify build.
    Former-commit-id: 3a80606d6c20751fa12c911fc965380b49086710

--- app/data/container-build/cerc-datanet-eth-geth/build.sh
+++ app/data/container-build/cerc-datanet-eth-geth/build.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 rm -rf $SCRIPT_DIR/build
 cp -rp $SCRIPT_DIR/../cerc-fixturenet-eth-geth $SCRIPT_DIR/build
 
-cp -f $SCRIPT_DIR/el-config.yaml $SCRIPT_DIR/build/genesis/el/el-config.yaml
+sed -i '' 's/^terminal_total_difficulty.*$/capped_maximum_difficulty: 1/' $SCRIPT_DIR/build/genesis/el/el-config.yaml
 
 docker build -t cerc/datanet-eth-geth:local ${SCRIPT_DIR}/build
 

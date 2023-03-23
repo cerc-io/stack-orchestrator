23fab04ec8cded42b11b4713ff32fd3d73af14ae
David Boreham <david@bozemanpas.com>
2023-03-23 16:09:27 -0600

    Fix filename
    Former-commit-id: 06288fd9e19b35e8b2e14a126ce52cecbe15d33e

--- app/data/container-build/cerc-foundry/build.sh
+++ app/data/container-build/cerc-foundry/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Build a local version of the foundry-rs/foundry image
-docker build -t cerc/foundry:local -f ${CERC_REPO_BASE_DIR}/Dockerfile.debian ${CERC_REPO_BASE_DIR}/foundry
+docker build -t cerc/foundry:local -f ${CERC_REPO_BASE_DIR}/foundry/Dockerfile-debian ${CERC_REPO_BASE_DIR}/foundry

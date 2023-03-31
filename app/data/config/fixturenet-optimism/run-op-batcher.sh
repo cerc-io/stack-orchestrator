0fa42a92e94d1ad6c7ca060354dd9dc5ab4ba763
nabarun <nabarun@deepstacksoft.com>
2023-03-31 13:39:31 +0530

    Implement op-node container

---
+++ app/data/config/fixturenet-optimism/run-op-batcher.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+# Get BACTHER_KEY from keys.json
+BATCHER_KEY=$(jq -r '.Batcher.privateKey' /l2-accounts/keys.json | tr -d '"')
+
+op-batcher \
+	--l2-eth-rpc=http://op-geth:8545 \
+	--rollup-rpc=http://op-node:8547 \
+	--poll-interval=1s \
+	--sub-safety-margin=6 \
+	--num-confirmations=1 \
+	--safe-abort-nonce-too-low-count=3 \
+	--resubmission-timeout=30s \
+	--rpc.addr=0.0.0.0 \
+	--rpc.port=8548 \
+	--rpc.enable-admin \
+	--max-channel-duration=1 \
+	--target-l1-tx-size-bytes=2048 \
+	--l1-eth-rpc=$L1_RPC \
+	--private-key=$BATCHER_KEY
--- app/data/config/fixturenet-optimism/run-op-node.sh
+++ app/data/config/fixturenet-optimism/run-op-node.sh
@@ -2,15 +2,15 @@
 set -e
 
 # Get SEQUENCER KEY from keys.json
-SEQUENCER_KEY=`jq '.Sequencer.privateKey' /l2-accounts/keys.json`
+SEQUENCER_KEY=$(jq -r '.Sequencer.privateKey' /l2-accounts/keys.json | tr -d '"')
 
 op-node \
 	--l2=http://op-geth:8551 \
-	--l2.jwt-secret=./jwt.txt \
+	--l2.jwt-secret=/app/jwt.txt \
 	--sequencer.enabled \
 	--sequencer.l1-confs=3 \
 	--verifier.l1-confs=3 \
-	--rollup.config=./rollup.json \
+	--rollup.config=/app/rollup.json \
 	--rpc.addr=0.0.0.0 \
 	--rpc.port=8547 \
 	--p2p.disable \

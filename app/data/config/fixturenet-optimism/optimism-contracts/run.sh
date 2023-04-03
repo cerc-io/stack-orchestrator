a022d94d1f498d4a59c4c90ffae89be18cd500bb
prathamesh0 <prathamesh.musale0@gmail.com>
2023-04-03 17:28:37 +0530

    Skip lighthouse node dependency check

--- app/data/config/fixturenet-optimism/optimism-contracts/run.sh
+++ app/data/config/fixturenet-optimism/optimism-contracts/run.sh
@@ -15,7 +15,7 @@ echo "import './rekey-json'" >> tasks/index.ts
 echo "import './send-balance'" >> tasks/index.ts
 
 # Update the chainId in the hardhat config
-sed -i "/getting-started/ {n; s/.*chainId.*/      chainId: $CHAIN_ID,/}" hardhat.config.ts
+sed -i "/getting-started/ {n; s/.*chainId.*/      chainId: $L1_CHAIN_ID,/}" hardhat.config.ts
 
 # Generate the L2 account addresses
 yarn hardhat rekey-json --output /l2-accounts/keys.json
@@ -41,13 +41,6 @@ else
   echo "Using L1 account credentials from env"
 fi
 
-# Send balances to the above L2 addresses
-yarn hardhat send-balance --to "${ADMIN_ADDRESS}" --amount 2 --private-key "${L1_PRIV_KEY}" --network getting-started
-yarn hardhat send-balance --to "${PROPOSER_ADDRESS}" --amount 5 --private-key "${L1_PRIV_KEY}" --network getting-started
-yarn hardhat send-balance --to "${BATCHER_ADDRESS}" --amount 1000 --private-key "${L1_PRIV_KEY}" --network getting-started
-
-echo "Balances sent to L2 accounts"
-
 # Select a finalized L1 block as the starting point for roll ups
 until FINALIZED_BLOCK=$(cast block finalized --rpc-url "$L1_RPC"); do
     echo "Waiting for a finalized L1 block to exist, retrying after 10s"
@@ -57,9 +50,16 @@ done
 L1_BLOCKHASH=$(echo "$FINALIZED_BLOCK" | awk '/hash/{print $2}')
 L1_BLOCKTIMESTAMP=$(echo "$FINALIZED_BLOCK" | awk '/timestamp/{print $2}')
 
+# Send balances to the above L2 addresses
+yarn hardhat send-balance --to "${ADMIN_ADDRESS}" --amount 2 --private-key "${L1_PRIV_KEY}" --network getting-started
+yarn hardhat send-balance --to "${PROPOSER_ADDRESS}" --amount 5 --private-key "${L1_PRIV_KEY}" --network getting-started
+yarn hardhat send-balance --to "${BATCHER_ADDRESS}" --amount 1000 --private-key "${L1_PRIV_KEY}" --network getting-started
+
+echo "Balances sent to L2 accounts"
+
 # Update the deployment config
 sed -i 's/"l2OutputOracleStartingTimestamp": TIMESTAMP/"l2OutputOracleStartingTimestamp": '"$L1_BLOCKTIMESTAMP"'/g' deploy-config/getting-started.json
-jq --arg chainid "$CHAIN_ID" '.l1ChainID = ($chainid | tonumber)' deploy-config/getting-started.json > tmp.json && mv tmp.json deploy-config/getting-started.json
+jq --arg chainid "$L1_CHAIN_ID" '.l1ChainID = ($chainid | tonumber)' deploy-config/getting-started.json > tmp.json && mv tmp.json deploy-config/getting-started.json
 
 node update-config.js deploy-config/getting-started.json "$ADMIN_ADDRESS" "$PROPOSER_ADDRESS" "$BATCHER_ADDRESS" "$SEQUENCER_ADDRESS" "$L1_BLOCKHASH"
 

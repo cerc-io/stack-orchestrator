abe0eaef6c7f70f0b37408e7d799a1a1e79399be
prathamesh0 <prathamesh.musale0@gmail.com>
2023-04-03 12:01:46 +0530

    Fix indentation

--- app/data/config/fixturenet-optimism/run-op-geth.sh
+++ app/data/config/fixturenet-optimism/run-op-geth.sh
@@ -30,29 +30,29 @@ SEQUENCER_ADDRESS=$(jq -r '.Sequencer.address' /l2-accounts/keys.json | tr -d '"
 echo "SEQUENCER_ADDRESS: ${SEQUENCER_ADDRESS}"
 cp /op-node/jwt.txt ./
 geth \
-	--datadir ./datadir \
-	--http \
-	--http.corsdomain="*" \
-	--http.vhosts="*" \
-	--http.addr=0.0.0.0 \
-	--http.api=web3,debug,eth,txpool,net,engine \
-	--ws \
-	--ws.addr=0.0.0.0 \
-	--ws.port=8546 \
-	--ws.origins="*" \
-	--ws.api=debug,eth,txpool,net,engine \
-	--syncmode=full \
-	--gcmode=full \
-	--nodiscover \
-	--maxpeers=0 \
-	--networkid=42069 \
-	--authrpc.vhosts="*" \
-	--authrpc.addr=0.0.0.0 \
-	--authrpc.port=8551 \
-	--authrpc.jwtsecret=./jwt.txt \
-	--rollup.disabletxpoolgossip=true \
-	--password=./datadir/password \
-	--allow-insecure-unlock \
-	--mine \
-	--miner.etherbase=$SEQUENCER_ADDRESS \
-	--unlock=$SEQUENCER_ADDRESS
+  --datadir ./datadir \
+  --http \
+  --http.corsdomain="*" \
+  --http.vhosts="*" \
+  --http.addr=0.0.0.0 \
+  --http.api=web3,debug,eth,txpool,net,engine \
+  --ws \
+  --ws.addr=0.0.0.0 \
+  --ws.port=8546 \
+  --ws.origins="*" \
+  --ws.api=debug,eth,txpool,net,engine \
+  --syncmode=full \
+  --gcmode=full \
+  --nodiscover \
+  --maxpeers=0 \
+  --networkid=42069 \
+  --authrpc.vhosts="*" \
+  --authrpc.addr=0.0.0.0 \
+  --authrpc.port=8551 \
+  --authrpc.jwtsecret=./jwt.txt \
+  --rollup.disabletxpoolgossip=true \
+  --password=./datadir/password \
+  --allow-insecure-unlock \
+  --mine \
+  --miner.etherbase=$SEQUENCER_ADDRESS \
+  --unlock=$SEQUENCER_ADDRESS
--- app/data/config/fixturenet-optimism/run-op-node.sh
+++ app/data/config/fixturenet-optimism/run-op-node.sh
@@ -5,16 +5,16 @@ set -e
 SEQUENCER_KEY=$(jq -r '.Sequencer.privateKey' /l2-accounts/keys.json | tr -d '"')
 
 op-node \
-	--l2=http://op-geth:8551 \
-	--l2.jwt-secret=/app/jwt.txt \
-	--sequencer.enabled \
-	--sequencer.l1-confs=3 \
-	--verifier.l1-confs=3 \
-	--rollup.config=/app/rollup.json \
-	--rpc.addr=0.0.0.0 \
-	--rpc.port=8547 \
-	--p2p.disable \
-	--rpc.enable-admin \
-	--p2p.sequencer.key=$SEQUENCER_KEY \
-	--l1=$L1_RPC \
-	--l1.rpckind=any
+  --l2=http://op-geth:8551 \
+  --l2.jwt-secret=/app/jwt.txt \
+  --sequencer.enabled \
+  --sequencer.l1-confs=3 \
+  --verifier.l1-confs=3 \
+  --rollup.config=/app/rollup.json \
+  --rpc.addr=0.0.0.0 \
+  --rpc.port=8547 \
+  --p2p.disable \
+  --rpc.enable-admin \
+  --p2p.sequencer.key=$SEQUENCER_KEY \
+  --l1=$L1_RPC \
+  --l1.rpckind=any

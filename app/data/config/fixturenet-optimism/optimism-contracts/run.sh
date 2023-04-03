cf807ce064276fe362e685bc810efde09767198c
prathamesh0 <prathamesh.musale0@gmail.com>
2023-04-03 15:54:02 +0530

    Replace fixturenet-eth dependency with wait on endpoint

--- app/data/config/fixturenet-optimism/optimism-contracts/run.sh
+++ app/data/config/fixturenet-optimism/optimism-contracts/run.sh
@@ -8,6 +8,8 @@ set -e -x
 #     exit 0
 # fi
 
+export L1_RPC="http://${L1_HOST}:${L1_PORT}"
+
 # Append tasks/index.ts file
 echo "import './rekey-json'" >> tasks/index.ts
 echo "import './send-balance'" >> tasks/index.ts
@@ -29,11 +31,15 @@ BATCHER_ADDRESS=$(echo "$KEYS_JSON" | jq -r '.Batcher.address')
 SEQUENCER_ADDRESS=$(echo "$KEYS_JSON" | jq -r '.Sequencer.address')
 
 # Read the private key of L1 accounts
-# TODO: Take from env if /geth-accounts volume doesn't exist to allow using separately running L1
-L1_ADDRESS=$(head -n 1 /geth-accounts/accounts.csv | cut -d ',' -f 2)
-L1_PRIV_KEY=$(head -n 1 /geth-accounts/accounts.csv | cut -d ',' -f 3)
-L1_ADDRESS_2=$(awk -F, 'NR==2{print $(NF-1)}' /geth-accounts/accounts.csv)
-L1_PRIV_KEY_2=$(awk -F, 'NR==2{print $NF}' /geth-accounts/accounts.csv)
+if [ -f /geth-accounts/accounts.csv ]; then
+  echo "Using L1 account credentials from the mounted volume"
+  L1_ADDRESS=$(head -n 1 /geth-accounts/accounts.csv | cut -d ',' -f 2)
+  L1_PRIV_KEY=$(head -n 1 /geth-accounts/accounts.csv | cut -d ',' -f 3)
+  L1_ADDRESS_2=$(awk -F, 'NR==2{print $(NF-1)}' /geth-accounts/accounts.csv)
+  L1_PRIV_KEY_2=$(awk -F, 'NR==2{print $NF}' /geth-accounts/accounts.csv)
+else
+  echo "Using L1 account credentials from env"
+fi
 
 # Send balances to the above L2 addresses
 yarn hardhat send-balance --to "${ADMIN_ADDRESS}" --amount 2 --private-key "${L1_PRIV_KEY}" --network getting-started

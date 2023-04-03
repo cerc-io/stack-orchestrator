f23c22d5b9cb170de93dea17d4a26931e311cd1a
Nabarun Gogoi <nabarun@deepstacksoft.com>
2023-04-03 18:13:29 +0530

    Replace laconicd with optimism in mobymask-v2 stack (#272)
    * Remove laconicd to use optimism endpoint
    
    * Use fixturenet-optimism stack for mobymask-v2-watcher
    
    * Fix setting L1 account private key in mobymask-v2 stack
    
    Former-commit-id: b266ac78b4253ff655875922670797a6e56d578f

--- app/data/config/fixturenet-optimism/optimism-contracts/run.sh
+++ app/data/config/fixturenet-optimism/optimism-contracts/run.sh
@@ -28,10 +28,12 @@ PROPOSER_ADDRESS=$(echo "$KEYS_JSON" | jq -r '.Proposer.address')
 BATCHER_ADDRESS=$(echo "$KEYS_JSON" | jq -r '.Batcher.address')
 SEQUENCER_ADDRESS=$(echo "$KEYS_JSON" | jq -r '.Sequencer.address')
 
-# Read the private key of a L1 account
+# Read the private key of L1 accounts
 # TODO: Take from env if /geth-accounts volume doesn't exist to allow using separately running L1
 L1_ADDRESS=$(head -n 1 /geth-accounts/accounts.csv | cut -d ',' -f 2)
 L1_PRIV_KEY=$(head -n 1 /geth-accounts/accounts.csv | cut -d ',' -f 3)
+L1_ADDRESS_2=$(awk -F, 'NR==2{print $(NF-1)}' /geth-accounts/accounts.csv)
+L1_PRIV_KEY_2=$(awk -F, 'NR==2{print $NF}' /geth-accounts/accounts.csv)
 
 # Send balances to the above L2 addresses
 yarn hardhat send-balance --to "${ADMIN_ADDRESS}" --amount 2 --private-key "${L1_PRIV_KEY}" --network getting-started
@@ -69,13 +71,18 @@ yarn hardhat deploy --network getting-started
 
 echo "Deployed the L1 smart contracts"
 
-# Read Proxy contarct's JSON and get the address
+# Read Proxy contract's JSON and get the address
 PROXY_JSON=$(cat deployments/getting-started/Proxy__OVM_L1StandardBridge.json)
 PROXY_ADDRESS=$(echo "$PROXY_JSON" | jq -r '.address')
 
-# Send balance to the above L2 address
+# Send balance to the above Proxy contract in L1 for reflecting balance in L2
+# First account
 yarn hardhat send-balance --to "${PROXY_ADDRESS}" --amount 1 --private-key "${L1_PRIV_KEY}" --network getting-started
+# Second account
+yarn hardhat send-balance --to "${PROXY_ADDRESS}" --amount 1 --private-key "${L1_PRIV_KEY_2}" --network getting-started
 
 echo "Balance sent to Proxy L2 contract"
-echo "Use account ${L1_ADDRESS} to deploy contracts to L2"
+echo "Use following accounts for transactions in L2:"
+echo "${L1_ADDRESS}"
+echo "${L1_ADDRESS_2}"
 echo "Done"

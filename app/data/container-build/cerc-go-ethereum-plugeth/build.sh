823bb95340e90e4476cca76e25d2edd6ee05d1be
David Boreham <david@bozemanpass.com>
2023-04-04 10:13:58 -0600

    Experimental plugeth support
    Former-commit-id: ff0e23ce47acd3a09d228697243e3ca9e789f300

---
+++ app/data/container-build/cerc-go-ethereum-plugeth/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Build cerc/go-ethereum-plugeth
+docker build -t cerc/go-ethereum-plugeth:local ${CERC_REPO_BASE_DIR}/plugeth
---
+++ app/data/stacks/fixturenet-plugeth/README.md
@@ -0,0 +1,4 @@
+# fixturenet-plugeth
+
+Experimental stack to try out [plugeth](https://github.com/openrelayxyz/plugeth)
+

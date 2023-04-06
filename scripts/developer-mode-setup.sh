9e2b140e101b8545d248af62eb22e61f2e1235b9
David Boreham <david@bozemanpass.com>
2023-04-06 13:50:50 -0600

    Ubuntu install script (#289)
    * Copy of Zach's install script
    
    * Few small fixes after testing on DO droplet
    
    * Update scripts
    
    * Rename script
    
    * Add sudo notice
    
    * Fix typo
    
    * Fix another typo
    
    * Update docker instructions

---
+++ scripts/developer-mode-setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Script to automate the steps needed to make a cloned project repo runnable on the path
+# (beware of PATH having some other file with the same name ahead of ours)
+if [[ -n "$CERC_SCRIPT_DEBUG" ]]; then
+    set -x
+    echo PATH is $PATH
+fi
+python3 -m venv venv
+source ./venv/bin/activate
+pip install --editable .
+pip install shiv
--- scripts/first_time_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 -m venv venv
-source ./venv/bin/activate
-pip install --editable .
-pip install shiv
-shiv -c laconic-so -o laconic-so .
-./laconic-so --verbose --local-stack setup-repositories

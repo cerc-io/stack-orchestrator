86f13e9c6bcf8d7f22366b765bae774c809fa537
Nabarun Gogoi <nabarun@deepstacksoft.com>
2023-04-06 15:17:00 +0530

    Separate out watcher and web-apps in mobymask-v2 stack (#287)
    * Separate out watcher and web-apps in mobymask stack
    
    * Take L2 RPC endpoint from the env file
    
    * Changes to run watcher and mobymask web-app separately
    
    * Support running watcher without contract deployment and L2 txs
    
    * Remove duplicate mobymask params env
    
    * Add code comments
    
    * Add instructions for running web-apps separately
    
    * Self review fixes
    
    * Fix timeout for mobymask-app on watcher server
    
    ---------
    
    Co-authored-by: prathamesh0 <prathamesh.musale0@gmail.com>
    Former-commit-id: 6f781ae303915d7612fbc670b4f5f9b521b48121

--- app/data/config/watcher-mobymask-v2/mobymask-app-start.sh
+++ app/data/config/watcher-mobymask-v2/mobymask-app-start.sh
@@ -4,9 +4,22 @@ if [ -n "$CERC_SCRIPT_DEBUG" ]; then
   set -x
 fi
 
-# Merging config files to get deployed contract address
-jq -s '.[0] * .[1]' /app/src/mobymask-app-config.json /server/config.json > /app/src/config.json
+# Use config from mounted volume if available (when running web-app along with watcher stack)
+if [ -f /server/config.json ]; then
+  echo "Merging config for deployed contract from mounted volume"
+  # Merging config files to get deployed contract address
+  jq -s '.[0] * .[1]' /app/src/mobymask-app-config.json /server/config.json > /app/src/config.json
+else
+  echo "Setting deployed contract details from env"
 
-npm run build
+  # Set config values from environment variables
+  jq --arg address "$DEPLOYED_CONTRACT" \
+    --argjson chainId $CHAIN_ID \
+    --argjson relayNodes "$RELAY_NODES" \
+    '.address = $address | .chainId = $chainId | .relayNodes = $relayNodes' \
+    /app/src/mobymask-app-config.json > /app/src/config.json
+fi
+
+REACT_APP_WATCHER_URI="$APP_WATCHER_URL/graphql" npm run build
 
 serve -s build
--- app/data/config/watcher-mobymask-v2/start-server.sh
+++ app/data/config/watcher-mobymask-v2/start-server.sh
@@ -4,8 +4,15 @@ if [ -n "$CERC_SCRIPT_DEBUG" ]; then
   set -x
 fi
 
-# Assign deployed contract address from server config
-CONTRACT_ADDRESS=$(jq -r '.address' /server/config.json | tr -d '"')
+echo "Using L2 RPC endpoint ${L2_GETH_RPC}"
+
+# Use contract address from environment variable or set from config.json in mounted volume
+if [ -n "$DEPLOYED_CONTRACT" ]; then
+  CONTRACT_ADDRESS="${DEPLOYED_CONTRACT}"
+else
+  # Assign deployed contract address from server config (created by mobymask container after deploying contract)
+  CONTRACT_ADDRESS=$(jq -r '.address' /server/config.json | tr -d '"')
+fi
 
 if [ -f /geth-accounts/accounts.csv ]; then
   echo "Using L1 private key from the mounted volume"
@@ -15,11 +22,20 @@ else
   echo "Using PRIVATE_KEY_PEER from env"
 fi
 
-sed "s/REPLACE_WITH_PRIVATE_KEY/${PRIVATE_KEY_PEER}/" environments/watcher-config-template.toml > environments/local.toml
-sed -i "s/REPLACE_WITH_CONTRACT_ADDRESS/${CONTRACT_ADDRESS}/" environments/local.toml
+# Set private key and contract address for watcher peer txs to L2 only if PRIVATE_KEY_PEER variable is set
+if [ -n "$PRIVATE_KEY_PEER" ]; then
+  # Read in config template TOML file and modify it
+  CONTENT=$(cat environments/watcher-config-template.toml)
+  NEW_CONTENT=$(echo "$CONTENT" | sed -E "/\[metrics\]/i \\\n\n      [server.p2p.peer.l2TxConfig]\n        privateKey = \"${PRIVATE_KEY_PEER}\"\n        contractAddress = \"${CONTRACT_ADDRESS}\"\n")
 
-export L2_GETH_URL="http://${L2_GETH_HOST}:${L2_GETH_PORT}"
-sed -i 's|REPLACE_WITH_L2_GETH_URL|'"${L2_GETH_URL}"'|' environments/local.toml
+  # Write the modified content to a watcher config file
+  echo "$NEW_CONTENT" > environments/local.toml
+
+  sed -i 's|REPLACE_WITH_L2_GETH_RPC_ENDPOINT|'"${L2_GETH_RPC}"'|' environments/local.toml
+else
+  # Copy template config to watcher config without setting params for peer L2 txs
+  cp environments/watcher-config-template.toml environments/local.toml
+fi
 
 echo 'yarn server'
 yarn server

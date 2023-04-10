f90000d9cca91300e5acd5dcffecb50a9061da47
David Boreham <david@bozemanpass.com>
2023-04-10 06:43:23 -0600

    New build version scheme (#305)
    * Use separate build tag file
    
    * Implement new versioning scheme
    
    * Update workflow file
    
    Former-commit-id: 80bbbafeb6016441ec0420ccdcb480fd4afa844d

--- scripts/build_shiv_package.sh
+++ scripts/build_shiv_package.sh
@@ -1,4 +1,4 @@
 # Builds the shiv "package" for distribution
 mkdir -p ./package
-version_string=$( ./scripts/update_version_file.sh)
+version_string=$( ./app/data/version.txt )
 shiv -c laconic-so -o package/laconic-so-${version_string} .
---
+++ scripts/create_build_tag_file.sh
@@ -0,0 +1,8 @@
+build_tag_file_name=./app/data/build_tag.txt
+echo "# This file should be re-generated running: scripts/create_build_tag_file.sh script" > $build_tag_file_name
+product_version_string=$( tail -1 ./app/data/version.txt )
+commit_string=$( git rev-parse --short HEAD )
+timestamp_string=$(date +'%Y%m%d%H%M')
+build_tag_string=${product_version_string}-${commit_string}-${timestamp_string}
+echo ${build_tag_string} >> $build_tag_file_name
+echo ${build_tag_string}

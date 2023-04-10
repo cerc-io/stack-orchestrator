3c5826d4b3ca35e4798d85f3c52f74e966590e54
David Boreham <david@bozemanpass.com>
2023-04-10 06:37:24 -0600

    Implement new versioning scheme

--- scripts/create_build_tag_file.sh
+++ scripts/create_build_tag_file.sh
@@ -1,7 +1,8 @@
-version_file_name=./app/data/version.txt
-echo "# This file should be re-generated running: scripts/update-version-file.sh script" > $version_file_name
-tag_string=$( git describe --tags --abbrev=0 )
+build_tag_file_name=./app/data/build_tag.txt
+echo "# This file should be re-generated running: scripts/create_build_tag_file.sh script" > $build_tag_file_name
+product_version_string=$( tail -1 ./app/data/version.txt )
 commit_string=$( git rev-parse --short HEAD )
-version_string=${tag_string}-${commit_string}
-echo ${version_string} >> $version_file_name
-echo ${version_string}
+timestamp_string=$(date +'%Y%m%d%H%M')
+build_tag_string=${product_version_string}-${commit_string}-${timestamp_string}
+echo ${build_tag_string} >> $build_tag_file_name
+echo ${build_tag_string}

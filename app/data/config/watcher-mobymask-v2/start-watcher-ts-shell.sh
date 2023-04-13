178570f8f429379dc56a3dfe538f2e6e08ef67ce
nabarun <nabarun@deepstacksoft.com>
2023-04-13 20:15:53 +0530

    Use standalone mobymask-v2-watcher-ts to run peer test

---
+++ app/data/config/watcher-mobymask-v2/start-watcher-ts-shell.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e
+if [ -n "$CERC_SCRIPT_DEBUG" ]; then
+  set -x
+fi
+
+# TODO: Build in Dockerfile
+apk --update --no-cache add jq
+yarn build
+
+# Check for peer ids in ./peers folder, create if not present
+if [ -f ./relay-id.json ]; then
+  echo "Using peer id for relay node from the mounted volume"
+else
+  echo "Creating a new peer id for relay node"
+  yarn create-peer -f relay-id.json
+fi
+
+if [ -f ./peer-id.json ]; then
+  echo "Using peer id for peer node from the mounted volume"
+else
+  echo "Creating a new peer id for peer node"
+  yarn create-peer -f peer-id.json
+fi
+
+CERC_RELAY_MULTIADDR="/dns4/mobymask-watcher-server/tcp/9090/ws/p2p/$(jq -r '.id' ./relay-id.json)"
+
+# Write the relay node's multiaddr to /app/packages/peer/.env for running tests
+echo "RELAY=\"$CERC_RELAY_MULTIADDR\"" > ./.env
+
+# TODO: Discuss leave shell running
+tail -f .env

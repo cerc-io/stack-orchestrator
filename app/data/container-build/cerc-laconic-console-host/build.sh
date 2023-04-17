c715e11a88b558afac6db93386d6277aa101b840
David Boreham <david@bozemanpass.com>
2023-04-17 13:40:49 -0600

    Use CERC_NPM_REGISTRY_URL everywhere (#340)
    Former-commit-id: 39a54bc62a90c9fdf5e7b53e9c9cfaba656d5d8a

--- app/data/container-build/cerc-laconic-console-host/build.sh
+++ app/data/container-build/cerc-laconic-console-host/build.sh
@@ -8,4 +8,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 docker build -t cerc/laconic-console-host:local ${build_command_args} -f ${SCRIPT_DIR}/Dockerfile \
   --add-host gitea.local:host-gateway \
-  --build-arg CERC_NPM_AUTH_TOKEN --build-arg CERC_NPM_URL ${SCRIPT_DIR}
+  --build-arg CERC_NPM_AUTH_TOKEN --build-arg CERC_NPM_REGISTRY_URL ${SCRIPT_DIR}

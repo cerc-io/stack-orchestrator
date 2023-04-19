c148922658e1e34d5eeb956f44896968144bdccd
David Boreham <david@bozemanpass.com>
2023-04-19 14:54:27 -0600

    Fix build script

--- scripts/build_shiv_package.sh
+++ scripts/build_shiv_package.sh
@@ -1,4 +1,4 @@
 # Builds the shiv "package" for distribution
 mkdir -p ./package
-version_string=$( ./app/data/version.txt )
+version_string=$( ./scripts/create_build_tag_file.sh )
 shiv -c laconic-so -o package/laconic-so-${version_string} .

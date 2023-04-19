2d5abdce45159c80b8a8012af5bb38f3812fbba4
David Boreham <david@bozemanpass.com>
2023-04-19 16:00:41 -0600

    Set context dir to the script dir to avoid permission errors (#354)
    Former-commit-id: bde48b699d57ba1e7648fee7fe0935eb8ba21347

--- app/data/container-build/cerc-act-runner-task-executor/build.sh
+++ app/data/container-build/cerc-act-runner-task-executor/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Build a local version of the task executor for act-runner
 source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
-docker build -t cerc/act-runner-task-executor:local -f ${CERC_REPO_BASE_DIR}/hosting/gitea/Dockerfile.task-executor ${build_command_args} ${CERC_REPO_BASE_DIR}/hosting
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+docker build -t cerc/act-runner-task-executor:local -f ${CERC_REPO_BASE_DIR}/hosting/gitea/Dockerfile.task-executor ${build_command_args} ${SCRIPT_DIR}

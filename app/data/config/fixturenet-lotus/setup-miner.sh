3d26fa28675bc30b9f9560a2e4e40d28baecfff5
iskay <ian@knowable.vc>
2023-05-08 16:42:20 +0000

    manual peer connect

---
+++ app/data/config/fixturenet-lotus/setup-miner.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+lotus --version
+#lotus daemon  --genesis=/devgen.car --profile=bootstrapper --bootstrap=false > /var/log/lotus.log 2>&1
+lotus daemon  --genesis=/devgen.car --bootstrap=false

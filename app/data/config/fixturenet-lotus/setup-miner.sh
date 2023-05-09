a6a13991382fa29eef7a79d390cd1359cc9dbd4f
iskay <ian@knowable.vc>
2023-05-09 19:09:19 +0000

    add shared volume

--- app/data/config/fixturenet-lotus/setup-miner.sh
+++ app/data/config/fixturenet-lotus/setup-miner.sh
@@ -2,4 +2,8 @@
 
 lotus --version
 #lotus daemon  --genesis=/devgen.car --profile=bootstrapper --bootstrap=false > /var/log/lotus.log 2>&1
-lotus daemon  --genesis=/devgen.car --bootstrap=false
+#lotus daemon  --genesis=/devgen.car --bootstrap=false
+
+nohup lotus daemon  --genesis=/devgen.car --profile=bootstrapper --bootstrap=false > /var/log/lotus.log 2>&1 &
+
+# initialization check here

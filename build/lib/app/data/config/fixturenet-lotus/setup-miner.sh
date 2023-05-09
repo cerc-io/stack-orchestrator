20884d80478a211dac86423ecb5031ab39f38fb0
iskay <ian@knowable.vc>
2023-05-09 22:56:44 +0000

    connect to bootnode

--- build/lib/app/data/config/fixturenet-lotus/setup-miner.sh
+++ build/lib/app/data/config/fixturenet-lotus/setup-miner.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
 lotus --version
-#lotus daemon  --genesis=/devgen.car --profile=bootstrapper --bootstrap=false > /var/log/lotus.log 2>&1
-lotus daemon  --genesis=/devgen.car --bootstrap=false
+
+nohup lotus daemon  --genesis=/devgen.car --profile=bootstrapper --bootstrap=false > /var/log/lotus.log 2>&1 &
+
+# Loop until the daemon is started
+echo "Waiting for daemon to start..."
+while ! grep -q "started ChainNotify channel" /var/log/lotus.log ; do
+    sleep 5
+done
+echo "Daemon started."
+
+# if not already initialized
+if [ ! -f /root/.lotus-shared/miner.addr ]; then
+  # initialize miner
+  lotus wallet import --as-default ~/.genesis-sectors/pre-seal-t01000.key
+  lotus-miner init --genesis-miner --actor=t01000 --sector-size=2KiB --pre-sealed-sectors=~/.genesis-sectors --pre-sealed-metadata=~/.genesis-sectors/pre-seal-t01000.json --nosync
+  
+  # publish miner address to shared volume
+  lotus net listen | awk 'NR==1{print}' > /root/.lotus-shared/miner.addr
+fi
+
+nohup lotus-miner run --nosync &
+
+tail -f /dev/null
--- build/lib/app/data/config/fixturenet-lotus/setup-node.sh
+++ build/lib/app/data/config/fixturenet-lotus/setup-node.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
 lotus --version
-lotus daemon  --genesis=/devgen.car
+
+##TODO: paths can use values from lotus-env.env file
+
+# if not already initialized
+if [ ! -f /root/.lotus-local-net/config.toml ]; then
+  # init node config
+  mkdir $HOME/.lotus-local-net
+  lotus config default > $HOME/.lotus-local-net/config.toml
+
+  # add bootstrap peer info if available
+  if [ -f /root/.lotus-shared/miner.addr ]; then
+    MINER_ADDR=\"$(cat /root/.lotus-shared/miner.addr)\"
+    # add bootstrap peer id to config file
+    sed -i "/^\[Libp2p\]/a \ \ BootstrapPeers = [$MINER_ADDR]" $HOME/.lotus-local-net/config.toml
+  else
+    echo "Bootstrap peer info not found, unable to configure. Manual peering will be required."
+  fi
+fi
+
+# start node
+lotus daemon --genesis=/devgen.car

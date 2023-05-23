6be132fe17ede2b2e34096967334a04279e74545
iskay <ikay@lakeheadu.ca>
2023-05-23 21:17:33 +0000

    remove pre-built genesis

--- app/data/config/fixturenet-lotus/setup-miner.sh
+++ app/data/config/fixturenet-lotus/setup-miner.sh
@@ -7,17 +7,13 @@ if [ -f /root/.lotus-shared/miner.addr ]; then
   rm /root/.lotus-shared/miner.addr
 fi
 
-
-##TODO: generate genesis files inside container instead of bundling in config dir
-##something like commands below should work, other scripts/compose will have to be updated to corresponding directories
-# lotus fetch-params 2048
-# lotus-seed pre-seal --sector-size 2KiB --num-sectors 2
-# lotus-seed genesis new localnet.json
-# lotus-seed genesis add-miner localnet.json ~/.genesis-sectors/pre-seal-t01000.json
-
+lotus fetch-params 2048
+lotus-seed pre-seal --sector-size 2KiB --num-sectors 2
+lotus-seed genesis new localnet.json
+lotus-seed genesis add-miner localnet.json ~/.genesis-sectors/pre-seal-t01000.json
 
 # start daemon
-nohup lotus daemon  --genesis=/devgen.car --profile=bootstrapper --bootstrap=false > /var/log/lotus.log 2>&1 &
+nohup lotus daemon --lotus-make-genesis=devgen.car --profile=bootstrapper --genesis-template=localnet.json --bootstrap=false > /var/log/lotus.log 2>&1 &
 
 # Loop until the daemon is started
 echo "Waiting for daemon to start..."
@@ -26,11 +22,14 @@ while ! grep -q "started ChainNotify channel" /var/log/lotus.log ; do
 done
 echo "Daemon started."
 
+# copy genesis file to shared volume
+cp /devgen.car /root/.lotus-shared
+
 # publish bootnode peer info to shared volume
 lotus net listen | awk 'NR==1{print}' > /root/.lotus-shared/miner.addr
 
 # if miner not already initialized
-if [ ! -d /root/.lotus-miner-local-net ]; then
+if [ ! -d $LOTUS_MINER_PATH ]; then
   # initialize miner
   lotus wallet import --as-default ~/.genesis-sectors/pre-seal-t01000.key
   lotus-miner init --genesis-miner --actor=t01000 --sector-size=2KiB --pre-sealed-sectors=~/.genesis-sectors --pre-sealed-metadata=~/.genesis-sectors/pre-seal-t01000.json --nosync
--- app/data/config/fixturenet-lotus/setup-node.sh
+++ app/data/config/fixturenet-lotus/setup-node.sh
@@ -2,8 +2,6 @@
 
 lotus --version
 
-##TODO: paths can use values from lotus-env.env file
-
 # Loop until the daemon is started
 echo "Waiting for miner to share peering info..."
 while [ ! -f /root/.lotus-shared/miner.addr ]; do
@@ -12,20 +10,20 @@ done
 echo "Resuming..."
 
 # if not already initialized
-if [ ! -f /root/.lotus-local-net/config.toml ]; then
+if [ ! -f $LOTUS_PATH/config.toml ]; then
   # init node config
-  mkdir $HOME/.lotus-local-net
-  lotus config default > $HOME/.lotus-local-net/config.toml
+  mkdir $LOTUS_PATH
+  lotus config default > $LOTUS_PATH/config.toml
 
   # add bootstrap peer info if available
   if [ -f /root/.lotus-shared/miner.addr ]; then
     MINER_ADDR=\"$(cat /root/.lotus-shared/miner.addr)\"
     # add bootstrap peer id to config file
-    sed -i "/^\[Libp2p\]/a \ \ BootstrapPeers = [$MINER_ADDR]" $HOME/.lotus-local-net/config.toml
+    sed -i "/^\[Libp2p\]/a \ \ BootstrapPeers = [$MINER_ADDR]" $LOTUS_PATH/config.toml
   else
     echo "Bootstrap peer info not found, unable to configure. Manual peering will be required."
   fi
 fi
 
 # start node
-lotus daemon --genesis=/devgen.car
+lotus daemon --genesis=/root/.lotus-shared/devgen.car

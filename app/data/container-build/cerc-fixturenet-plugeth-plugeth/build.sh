23a336020caabf764944af44de0f264f9fdfc758
Thomas E Lackey <telackey@bozemanpass.com>
2023-05-26 16:57:15 -0500

    Make a separate lighthouse container for the plugeth fixturenet. (#412)
    * Make a separate lighthouse container for the plugeth fixturenet.

---
+++ app/data/container-build/cerc-fixturenet-plugeth-plugeth/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Build cerc/fixturenet-plugeth-plugeth
+set -x
+
+source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ ! -d "${SCRIPT_DIR}/genesis" ]; then
+  cp -frp ${SCRIPT_DIR}/../cerc-fixturenet-eth-geth/genesis ${SCRIPT_DIR}/genesis
+fi
+
+if [ ! -e "${SCRIPT_DIR}/run-el.sh" ]; then
+  cp -fp ${SCRIPT_DIR}/../cerc-fixturenet-eth-geth/run-el.sh ${SCRIPT_DIR}/
+fi
+
+docker build -t cerc/fixturenet-plugeth-plugeth:local -f ${SCRIPT_DIR}/Dockerfile ${build_command_args} $SCRIPT_DIR

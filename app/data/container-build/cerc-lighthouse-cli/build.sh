223aacb16ff1b66e5bc7d8616461c2086546dd9a
David Boreham <david@bozemanpas.com>
2023-06-04 14:30:01 -0600

    Enable portable lcli build

--- app/data/container-build/cerc-lighthouse-cli/build.sh
+++ app/data/container-build/cerc-lighthouse-cli/build.sh
@@ -4,4 +4,4 @@
 source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
 
 project_dir=${CERC_REPO_BASE_DIR}/lighthouse
-docker build -t cerc/lighthouse-cli:local -f ${project_dir}/lcli/Dockerfile ${build_command_args} ${project_dir}
+docker build -t cerc/lighthouse-cli:local --build-arg PORTABLE=true -f ${project_dir}/lcli/Dockerfile ${build_command_args} ${project_dir}

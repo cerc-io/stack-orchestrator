b115e80f1202bd29cb245b3bb4569be4572b43f6
cerebralbosons <zach@bluecollarcoding.ca>
2023-06-23 18:23:19 +0000

    copy pasta

--- app/data/container-build/cerc-lasso/build.sh
+++ app/data/container-build/cerc-lasso/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# Build a local version of the foundry-rs/foundry image
+# Build the lasso image
 source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
 docker build -t cerc/lasso:local -f ${CERC_REPO_BASE_DIR}/lasso/Dockerfile ${build_command_args} ${CERC_REPO_BASE_DIR}/lasso

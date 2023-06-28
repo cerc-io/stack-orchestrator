5a2d8a3a2b371d865117cd010caf85e3132edfef
David Boreham <david@bozemanpass.com>
2023-06-28 12:11:36 -0600

    Specify the git tag/branch for opera in stack.yml

--- app/data/container-build/cerc-go-opera/build.sh
+++ app/data/container-build/cerc-go-opera/build.sh
@@ -2,10 +2,6 @@
 # Build cerc/go-opera
 source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
 
-# Checkout appropriate release; refer to https://docs.fantom.foundation/
-OPERA_TAG=${OPERA_TAG:-release/1.1.2-rc.5}
-git -C ${CERC_REPO_BASE_DIR}/go-opera checkout ${OPERA_TAG}
-
 # Repo's dockerfile gives build error because it's hardcoded for go 1.17; go 1.19 is required
 sed -i 's/FROM golang:1\.[0-9]*-alpine as builder/FROM golang:1.19-alpine as builder/' ${CERC_REPO_BASE_DIR}/go-opera/docker/Dockerfile.opera
 

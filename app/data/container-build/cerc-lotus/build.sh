61499004022acf09313592ebcb84df9edddad8f9
Nabarun <nabarun@deepstacksoft.com>
2023-08-10 13:38:15 +0530

    Use release version and add healthcheck in Lotus stack

--- app/data/container-build/cerc-lotus/build.sh
+++ app/data/container-build/cerc-lotus/build.sh
@@ -3,8 +3,8 @@
 source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Per lotus docs, 'releases' branch always contains latest stable release
-git -C ${CERC_REPO_BASE_DIR}/lotus checkout releases
+# Use a release version tag to match the modified Dockerfile replaced in next step
+git -C ${CERC_REPO_BASE_DIR}/lotus checkout v1.23.3
 
 # Replace repo's Dockerfile with modified one
 cp ${SCRIPT_DIR}/Dockerfile ${CERC_REPO_BASE_DIR}/lotus/Dockerfile

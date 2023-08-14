59e0458c7408b0e926a612eedb2f016f71549c41
prathamesh0 <42446521+prathamesh0@users.noreply.github.com>
2023-08-14 12:46:04 +0530

    Restart support for fixturenet-lotus (#499)
    * Use ip utility to get the required miner node multiaddr
    
    * Persist lotus node data to support restarts
    
    * Add clean up steps to instructions
    
    * Fix lotus-seed sector-dir arg

--- app/data/config/fixturenet-lotus/setup-node.sh
+++ app/data/config/fixturenet-lotus/setup-node.sh
@@ -9,20 +9,17 @@ while [ ! -f /root/.lotus-shared/miner.addr ]; do
 done
 echo "Resuming..."
 
-# if not already initialized
-if [ ! -f $LOTUS_PATH/config.toml ]; then
-  # init node config
-  mkdir $LOTUS_PATH
-  lotus config default > $LOTUS_PATH/config.toml
+# init node config
+mkdir -p $LOTUS_PATH
+lotus config default > $LOTUS_PATH/config.toml
 
-  # add bootstrap peer info if available
-  if [ -f /root/.lotus-shared/miner.addr ]; then
-    MINER_ADDR=\"$(cat /root/.lotus-shared/miner.addr)\"
-    # add bootstrap peer id to config file
-    sed -i "/^\[Libp2p\]/a \ \ BootstrapPeers = [$MINER_ADDR]" $LOTUS_PATH/config.toml
-  else
-    echo "Bootstrap peer info not found, unable to configure. Manual peering will be required."
-  fi
+# add bootstrap peer info if available
+if [ -f /root/.lotus-shared/miner.addr ]; then
+  MINER_ADDR=\"$(cat /root/.lotus-shared/miner.addr)\"
+  # add bootstrap peer id to config file
+  sed -i "/^\[Libp2p\]/a \ \ BootstrapPeers = [$MINER_ADDR]" $LOTUS_PATH/config.toml
+else
+  echo "Bootstrap peer info not found, unable to configure. Manual peering will be required."
 fi
 
 # start node

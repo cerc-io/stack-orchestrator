f55a14bd6cb6d00c422ef07a09a86b7c91af9e38
Thomas E Lackey <telackey@bozemanpass.com>
2023-08-22 13:32:45 -0500

    Create new base container for pre-compiled webapps. (#507)
    * Create new base container for pre-compiled webapps.
    
    * Set the webapp dir appropriately.

--- app/data/container-build/cerc-laconic-console-host/start-serving-app.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if [ -n "$CERC_SCRIPT_DEBUG" ]; then
-    set -x
-fi
-# TODO: Don't hard wire this:
-webapp_files_dir=/usr/local/share/.config/yarn/global/node_modules/@cerc-io/console-app/dist/production
-/scripts/apply-webapp-config.sh /config/config.yml ${webapp_files_dir}
-http-server -p 80 ${webapp_files_dir}

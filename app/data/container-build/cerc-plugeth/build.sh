c30c7795353b1cd17278a1dd9442c4ae3d9a5c93
David Boreham <david@bozemanpass.com>
2023-09-08 15:37:59 -0600

    Pass through auth token to plugeth container builds (#529)

--- app/data/container-build/cerc-plugeth/build.sh
+++ app/data/container-build/cerc-plugeth/build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 # Build cerc/plugeth
 source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
-docker build -t cerc/plugeth:local ${build_command_args} ${CERC_REPO_BASE_DIR}/plugeth
+# This container build currently requires access to private dependencies in gitea
+# so we check that the necessary access token has been supplied here, then pass it o the build
+if [[ -z "${CERC_GO_AUTH_TOKEN}" ]]; then
+    echo "ERROR: CERC_GO_AUTH_TOKEN is not set" >&2
+    exit 1
+fi
+docker build -t cerc/plugeth:local ${build_command_args} --build-arg GIT_VDBTO_TOKEN=${CERC_GO_AUTH_TOKEN} ${CERC_REPO_BASE_DIR}/plugeth

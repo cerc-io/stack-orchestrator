7a37715038fd2692a6ee0718dc8b22b784318377
David Boreham <david@bozemanpas.com>
2023-09-10 13:18:43 -0600

    Update copyright messages

--- cli.py
+++ cli.py
@@ -1,4 +1,4 @@
-# Copyright © 2022 Cerc
+# Copyright © 2022, 2023 Vulcanize
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by

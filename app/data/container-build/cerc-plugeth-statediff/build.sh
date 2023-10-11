d1ea028340408e3bda2e8de9c9966072b2bd23fd
Roy Crihfield <roy@manteia.ltd>
2023-10-11 17:12:47 -0500

    doc

--- app/data/container-build/cerc-plugeth-statediff/build.sh
+++ app/data/container-build/cerc-plugeth-statediff/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Build cerc/plugeth-statediff
 source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
+# Pass Go auth token if present
 docker build -t cerc/plugeth-statediff:local ${build_command_args} --build-arg GIT_VDBTO_TOKEN=${CERC_GO_AUTH_TOKEN} ${CERC_REPO_BASE_DIR}/plugeth-statediff
--- app/data/container-build/cerc-plugeth/build.sh
+++ app/data/container-build/cerc-plugeth/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Build cerc/plugeth
 source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
+# Pass Go auth token if present
 docker build -t cerc/plugeth:local ${build_command_args} --build-arg GIT_VDBTO_TOKEN=${CERC_GO_AUTH_TOKEN} ${CERC_REPO_BASE_DIR}/plugeth

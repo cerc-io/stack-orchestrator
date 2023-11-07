438c02aa3540ff39a5cfbe1b4f80be22e33ce728
David Boreham <david@bozemanpass.com>
2023-11-06 22:06:39 -0700

    Fix build script for dir rename

--- scripts/create_build_tag_file.sh
+++ scripts/create_build_tag_file.sh
@@ -1,6 +1,6 @@
-build_tag_file_name=./app/data/build_tag.txt
+build_tag_file_name=./stack_orchestrator/data/build_tag.txt
 echo "# This file should be re-generated running: scripts/create_build_tag_file.sh script" > $build_tag_file_name
-product_version_string=$( tail -1 ./app/data/version.txt )
+product_version_string=$( tail -1 ./stack_orchestrator/data/version.txt )
 commit_string=$( git rev-parse --short HEAD )
 timestamp_string=$(date +'%Y%m%d%H%M')
 build_tag_string=${product_version_string}-${commit_string}-${timestamp_string}

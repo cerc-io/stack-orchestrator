3db443b2bbcc26342e9b7fa63518efef8754b036
iskay <ikay@lakeheadu.ca>
2023-11-10 20:42:02 +0000

    fix commands import

--- stack_orchestrator/data/stacks/fixturenet-optimism/deploy/commands.py
+++ stack_orchestrator/data/stacks/fixturenet-optimism/deploy/commands.py
@@ -12,7 +12,7 @@
 
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http:#www.gnu.org/licenses/>.
-from app.deploy.deploy_types import DeployCommandContext, LaconicStackSetupCommand, DeploymentContext
+from stack_orchestrator.deploy.deployment_context import DeploymentContext
 from ruamel.yaml import YAML
 
 def create(context: DeploymentContext, extra_args):

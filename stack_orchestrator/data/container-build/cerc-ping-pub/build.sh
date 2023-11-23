1ae144a9b82fe15d3abaa17fd8fab6564215324c
zramsay <zach@bluecollarcoding.ca>
2023-11-23 01:28:29 +0000

    fixes

--- stack_orchestrator/data/container-build/cerc-ping-pub/build.sh
+++ stack_orchestrator/data/container-build/cerc-ping-pub/build.sh
@@ -2,7 +2,4 @@
 # Build the ping pub image
 source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
 
-# See: https://stackoverflow.com/a/246128/1701505
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-docker build -t cerc/ping-pub:local ${build_command_args} -f ${SCRIPT_DIR}/Dockerfile $SCRIPT_DIR
+docker build -t cerc/ping-pub:local ${build_command_args} -f $CERC_REPO_BASE_DIR/explorer/Dockerfile $CERC_REPO_BASE_DIR/explorer

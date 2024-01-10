b83030f63b01311c32a43d4ad59adb2d241b1acf
Thomas E Lackey <telackey@bozemanpass.com>
2024-01-09 22:53:43 -0600

    Use custom act with gitea. (#700)
    * Use custom act with gitea.
    
    * Make sure wget is available
    
    * Fix repo url

--- stack_orchestrator/data/container-build/cerc-act-runner-task-executor/build.sh
+++ stack_orchestrator/data/container-build/cerc-act-runner-task-executor/build.sh
@@ -2,4 +2,6 @@
 # Build a local version of the task executor for act-runner
 source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-docker build -t cerc/act-runner-task-executor:local -f ${CERC_REPO_BASE_DIR}/hosting/act-runner/Dockerfile.task-executor ${build_command_args} ${SCRIPT_DIR}
+
+cd ${CERC_REPO_BASE_DIR}/hosting/act-runner
+docker build -t cerc/act-runner-task-executor:local -f Dockerfile.task-executor ${build_command_args} .

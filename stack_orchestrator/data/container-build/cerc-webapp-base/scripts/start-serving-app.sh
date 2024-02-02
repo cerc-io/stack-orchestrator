8fa7efc2c53ca63a715eaee80734aabdae343dc4
Thomas E Lackey <telackey@bozemanpass.com>
2024-02-02 14:13:48 -0600

    Support other webapp types (react, static).

--- stack_orchestrator/data/container-build/cerc-webapp-base/scripts/start-serving-app.sh
+++ stack_orchestrator/data/container-build/cerc-webapp-base/scripts/start-serving-app.sh
@@ -6,4 +6,5 @@ fi
 CERC_WEBAPP_FILES_DIR="${CERC_WEBAPP_FILES_DIR:-/data}"
 
 /scripts/apply-webapp-config.sh /config/config.yml ${CERC_WEBAPP_FILES_DIR}
-http-server -p 80 ${CERC_WEBAPP_FILES_DIR}
+/scripts/apply-runtime-env.sh ${CERC_WEBAPP_FILES_DIR}
+http-server --cors -p 3000 ${CERC_WEBAPP_FILES_DIR}

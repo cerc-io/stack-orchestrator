72ed2eb91abfc5db06a318013b0d3807a2604721
Thomas E Lackey <telackey@bozemanpass.com>
2024-02-08 20:38:41 +0000

    Fix bad test in tag check. (#738)
    Reviewed-on: https://git.vdb.to/cerc-io/stack-orchestrator/pulls/738
    Co-authored-by: Thomas E Lackey <telackey@bozemanpass.com>
    Co-committed-by: Thomas E Lackey <telackey@bozemanpass.com>

--- stack_orchestrator/deploy/webapp/util.py
+++ stack_orchestrator/deploy/webapp/util.py
@@ -355,11 +355,13 @@ def generate_hostname_for_app(app):
 
 def skip_by_tag(r, include_tags, exclude_tags):
     for tag in exclude_tags:
-        if tag and r.attributes.tags and tag in r.attributes.tags:
+        if r.attributes.tags and tag in r.attributes.tags:
             return True
 
-    for tag in include_tags:
-        if tag and (not r.attributes.tags or tag not in r.attributes.tags):
-            return True
+    if include_tags:
+        for tag in include_tags:
+            if r.attributes.tags and tag in r.attributes.tags:
+                return False
+        return True
 
     return False

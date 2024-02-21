db4986dcc65aa4e8b6500babc7030e47824d7026
Thomas E Lackey <telackey@bozemanpass.com>
2024-02-21 04:45:46 +0000

    snowballtool-base backend stack (#751)
    This adds a stack for the backend from snowball/snowballtools-base.
    
    Reviewed-on: https://git.vdb.to/cerc-io/stack-orchestrator/pulls/751
    Co-authored-by: Thomas E Lackey <telackey@bozemanpass.com>
    Co-committed-by: Thomas E Lackey <telackey@bozemanpass.com>

--- stack_orchestrator/data/container-build/cerc-webapp-base/scripts/start-serving-app.sh
+++ stack_orchestrator/data/container-build/cerc-webapp-base/scripts/start-serving-app.sh
@@ -10,6 +10,18 @@ if [ "true" == "$CERC_ENABLE_CORS" ]; then
   CERC_HTTP_EXTRA_ARGS="$CERC_HTTP_EXTRA_ARGS --cors"
 fi
 
-/scripts/apply-webapp-config.sh /config/config.yml ${CERC_WEBAPP_FILES_DIR}
+LACONIC_HOSTED_CONFIG_FILE=${LACONIC_HOSTED_CONFIG_FILE}
+if [ -z "${LACONIC_HOSTED_CONFIG_FILE}" ]; then
+  if [ -f "/config/laconic-hosted-config.yml" ]; then
+    LACONIC_HOSTED_CONFIG_FILE="/config/laconic-hosted-config.yml"
+  elif [ -f "/config/config.yml" ]; then
+    LACONIC_HOSTED_CONFIG_FILE="/config/config.yml"
+  fi
+fi
+
+if [ -f "${LACONIC_HOSTED_CONFIG_FILE}" ]; then
+  /scripts/apply-webapp-config.sh $LACONIC_HOSTED_CONFIG_FILE ${CERC_WEBAPP_FILES_DIR}
+fi
+
 /scripts/apply-runtime-env.sh ${CERC_WEBAPP_FILES_DIR}
 http-server $CERC_HTTP_EXTRA_ARGS -p ${CERC_LISTEN_PORT:-80} ${CERC_WEBAPP_FILES_DIR}

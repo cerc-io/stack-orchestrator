2a7a1c46b09110d25e4d681682592e12fb6c5e71
jonathan@vulcanize.io <jface@woodmere.vulc.ch2.20c.com>
2024-03-08 19:27:36 +0000

    fixed optimum deployment

--- stack_orchestrator/data/stacks/blast/deploy/commands.py
+++ stack_orchestrator/data/stacks/blast/deploy/commands.py
@@ -20,7 +20,7 @@ from shutil import copy
 
 def create(context, extra_args):
 
-    # Our goal here is just to copy the genesis.json file for blast
+    # Our goal here is just to copy the json files for blast
     deployment_config_dir = context.deployment_dir.joinpath("data", "blast-data")
     command_context = extra_args[2]
     compose_file = [f for f in command_context.cluster_context.compose_files if "blast" in f][0]

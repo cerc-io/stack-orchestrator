277fbae98ffc1f95487f92d868c1cec0f72972e4
jonathan@vulcanize.io <jonathan@vulcanize.io>
2024-04-26 15:09:24 +0000

    build fixes

--- stack_orchestrator/data/container-build/cerc-mainnet-lotus/build.sh
+++ stack_orchestrator/data/container-build/cerc-mainnet-lotus/build.sh
@@ -4,9 +4,9 @@ source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Use a release version tag to match the modified Dockerfile replaced in next step
-git -C ${CERC_REPO_BASE_DIR}/lotus checkout v1.26.3
+git -C ${CERC_REPO_BASE_DIR}/lotus checkout master
 
 # Replace repo's Dockerfile with modified one
 cp ${SCRIPT_DIR}/Dockerfile ${CERC_REPO_BASE_DIR}/lotus/Dockerfile
 
-docker build -t cerc/mainnet-lotus:local ${build_command_args} ${CERC_REPO_BASE_DIR}/lotus
+docker build -t cerc/lotus-mainnet:local ${build_command_args} ${CERC_REPO_BASE_DIR}/lotus

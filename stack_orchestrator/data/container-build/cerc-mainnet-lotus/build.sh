21f7b96eb4744b38ee2fcf7e9edcb21264d34a8e
jonathan@vulcanize.io <jonathan@vulcanize.io>
2024-05-03 19:53:21 +0000

    temp testing

--- stack_orchestrator/data/container-build/cerc-mainnet-lotus/build.sh
+++ stack_orchestrator/data/container-build/cerc-mainnet-lotus/build.sh
@@ -3,8 +3,10 @@
 source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+git stash
+
 # Use a release version tag to match the modified Dockerfile replaced in next step
-git -C ${CERC_REPO_BASE_DIR}/lotus checkout master
+git -C ${CERC_REPO_BASE_DIR}/lotus checkout v1.27.0-rc1-c
 
 # Replace repo's Dockerfile with modified one
 cp ${SCRIPT_DIR}/Dockerfile ${CERC_REPO_BASE_DIR}/lotus/Dockerfile

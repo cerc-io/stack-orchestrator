6928a6075103ce873b7dc96596f8b0eb1fe0abd4
Vivian Phung <dev+github@vivianphung.com>
2024-07-22 14:36:07 +0100

    Revert "nextjs config file options"
    This reverts commit b1659823fb98d2d79d59c9511f238a13c4f180df.

--- stack_orchestrator/data/container-build/cerc-nextjs-base/scripts/build-app.sh
+++ stack_orchestrator/data/container-build/cerc-nextjs-base/scripts/build-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -n "$CERC_SCRIPT_DEBUG" ]; then
-  set -x
+    set -x
 fi
 
 CERC_MIN_NEXTVER=13.4.2
@@ -20,21 +20,18 @@ if [ -z "$CERC_BUILD_TOOL" ]; then
   fi
 fi
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WORK_DIR="${1:-/app}"
 
 cd "${WORK_DIR}" || exit 1
 
 # If this file doesn't exist at all, we'll get errors below.
-NEXTJS_CONFIG_FILE="next.config.js"
-if [ -f "next.config.mjs" ]; then
-  CONFIG_FILE_TO_BEAUTIFY="next.config.mjs"
-elif [ ! -f "$NEXTJS_CONFIG_FILE" ]; then
-  touch "$NEXTJS_CONFIG_FILE"
+if [ ! -f "next.config.js" ]; then
+  touch next.config.js
 fi
 
 if [ ! -f "next.config.dist" ]; then
-  cp "$NEXTJS_CONFIG_FILE" next.config.dist
+  cp next.config.js next.config.dist
 fi
 
 which js-beautify >/dev/null
@@ -42,17 +39,17 @@ if [ $? -ne 0 ]; then
   npm i -g js-beautify
 fi
 
-js-beautify next.config.dist >"$NEXTJS_CONFIG_FILE"
-echo "" >>"$NEXTJS_CONFIG_FILE"
+js-beautify next.config.dist > next.config.js
+echo "" >> next.config.js
 
 WEBPACK_REQ_LINE=$(grep -n "require([\'\"]webpack[\'\"])" next.config.js | cut -d':' -f1)
 if [ -z "$WEBPACK_REQ_LINE" ]; then
-  cat >"$NEXTJS_CONFIG_FILE".0 <<EOF
+  cat > next.config.js.0 <<EOF
     const webpack = require('webpack');
 EOF
 fi
 
-cat >"$NEXTJS_CONFIG_FILE".1 <<EOF
+cat > next.config.js.1 <<EOF
 let envMap;
 try {
   // .env-list.json provides us a list of identifiers which should be replaced at runtime.
@@ -71,21 +68,21 @@ try {
 }
 EOF
 
-CONFIG_LINES=$(wc -l "$NEXTJS_CONFIG_FILE" | awk '{ print $1 }')
-ENV_LINE=$(grep -n 'env:' "$NEXTJS_CONFIG_FILE" | cut -d':' -f1)
-WEBPACK_CONF_LINE=$(egrep -n 'webpack:\s+\([^,]+,' "$NEXTJS_CONFIG_FILE" | cut -d':' -f1)
+CONFIG_LINES=$(wc -l next.config.js | awk '{ print $1 }')
+ENV_LINE=$(grep -n 'env:' next.config.js | cut -d':' -f1)
+WEBPACK_CONF_LINE=$(egrep -n 'webpack:\s+\([^,]+,' next.config.js | cut -d':' -f1)
 NEXT_SECTION_ADJUSTMENT=0
 
 if [ -n "$WEBPACK_CONF_LINE" ]; then
-  WEBPACK_CONF_VAR=$(egrep -n 'webpack:\s+\([^,]+,' "$NEXTJS_CONFIG_FILE" | cut -d',' -f1 | cut -d'(' -f2)
-  head -$((${WEBPACK_CONF_LINE})) "$NEXTJS_CONFIG_FILE" >"$NEXTJS_CONFIG_FILE".2
-  cat >"$NEXTJS_CONFIG_FILE".3 <<EOF
+  WEBPACK_CONF_VAR=$(egrep -n 'webpack:\s+\([^,]+,' next.config.js | cut -d',' -f1 | cut -d'(' -f2)
+  head -$(( ${WEBPACK_CONF_LINE} )) next.config.js > next.config.js.2
+  cat > next.config.js.3 <<EOF
       $WEBPACK_CONF_VAR.plugins.push(new webpack.DefinePlugin(envMap));
 EOF
   NEXT_SECTION_LINE=$((WEBPACK_CONF_LINE))
 elif [ -n "$ENV_LINE" ]; then
-  head -$((${ENV_LINE} - 1)) "$NEXTJS_CONFIG_FILE" >"$NEXTJS_CONFIG_FILE".2
-  cat >"$NEXTJS_CONFIG_FILE".3 <<EOF
+  head -$(( ${ENV_LINE} - 1 )) next.config.js > next.config.js.2
+  cat > next.config.js.3 <<EOF
     webpack: (config) => {
       config.plugins.push(new webpack.DefinePlugin(envMap));
       return config;
@@ -94,35 +91,35 @@ EOF
   NEXT_SECTION_ADJUSTMENT=1
   NEXT_SECTION_LINE=$ENV_LINE
 else
-  echo "WARNING: Cannot find location to insert environment variable map in "$NEXTJS_CONFIG_FILE"" 1>&2
-  rm -f "$NEXTJS_CONFIG_FILE".*
+  echo "WARNING: Cannot find location to insert environment variable map in next.config.js" 1>&2
+  rm -f next.config.js.*
   NEXT_SECTION_LINE=0
 fi
 
-tail -$((${CONFIG_LINES} - ${NEXT_SECTION_LINE} + ${NEXT_SECTION_ADJUSTMENT})) "$NEXTJS_CONFIG_FILE" >"$NEXTJS_CONFIG_FILE".5
+tail -$(( ${CONFIG_LINES} - ${NEXT_SECTION_LINE} + ${NEXT_SECTION_ADJUSTMENT} )) next.config.js > next.config.js.5
 
-cat "$NEXTJS_CONFIG_FILE".* | sed 's/^ *//g' | js-beautify | grep -v 'process\.\env\.' | js-beautify >"$NEXTJS_CONFIG_FILE"
-rm "$NEXTJS_CONFIG_FILE".*
+cat next.config.js.* | sed 's/^ *//g' | js-beautify | grep -v 'process\.\env\.' | js-beautify > next.config.js
+rm next.config.js.*
 
-"${SCRIPT_DIR}/find-env.sh" "$(pwd)" >.env-list.json
+"${SCRIPT_DIR}/find-env.sh" "$(pwd)" > .env-list.json
 
 if [ ! -f "package.dist" ]; then
   cp package.json package.dist
 fi
 
-cat package.dist | jq '.scripts.cerc_compile = "next experimental-compile"' | jq '.scripts.cerc_generate = "next experimental-generate"' >package.json
+cat package.dist | jq '.scripts.cerc_compile = "next experimental-compile"' | jq '.scripts.cerc_generate = "next experimental-generate"' > package.json
 
-CUR_NEXT_VERSION="$(jq -r '.dependencies.next' package.json)"
+CUR_NEXT_VERSION="`jq -r '.dependencies.next' package.json`"
 
 if [ "$CERC_NEXT_VERSION" != "keep" ] && [ "$CUR_NEXT_VERSION" != "$CERC_NEXT_VERSION" ]; then
   echo "Changing 'next' version specifier from '$CUR_NEXT_VERSION' to '$CERC_NEXT_VERSION' (set with '--extra-build-args \"--build-arg CERC_NEXT_VERSION=$CERC_NEXT_VERSION\"')"
-  cat package.json | jq ".dependencies.next = \"$CERC_NEXT_VERSION\"" >package.json.$$
+  cat package.json | jq ".dependencies.next = \"$CERC_NEXT_VERSION\"" > package.json.$$
   mv package.json.$$ package.json
 fi
 
 time $CERC_BUILD_TOOL install || exit 1
 
-CUR_NEXT_VERSION=$(jq -r '.version' node_modules/next/package.json)
+CUR_NEXT_VERSION=`jq -r '.version' node_modules/next/package.json`
 
 semver -p -r ">=$CERC_MIN_NEXTVER" $CUR_NEXT_VERSION
 if [ $? -ne 0 ]; then
@@ -141,7 +138,7 @@ to use for the build with:
 ###############################################################################
 
 EOF
-  cat package.json | jq ".dependencies.next = \"^$CERC_MIN_NEXTVER\"" >package.json.$$
+  cat package.json | jq ".dependencies.next = \"^$CERC_MIN_NEXTVER\"" > package.json.$$
   mv package.json.$$ package.json
   time $CERC_BUILD_TOOL install || exit 1
 fi

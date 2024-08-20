44b97097179f31e814dc018b6bb0b4553129df5e
David Boreham <david@bozemanpass.com>
2024-08-20 17:44:00 +0000

    Use Laconic version of ping-pub (#930)
    Reviewed-on: https://git.vdb.to/cerc-io/stack-orchestrator/pulls/930
    Co-authored-by: David Boreham <david@bozemanpass.com>
    Co-committed-by: David Boreham <david@bozemanpass.com>

--- stack_orchestrator/data/container-build/cerc-ping-pub/build.sh
+++ stack_orchestrator/data/container-build/cerc-ping-pub/build.sh
@@ -4,5 +4,9 @@ source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Two-stage build is to allow us to pick up both the upstream repo's files, and local files here for config
-docker build -t cerc/ping-pub-base:local ${build_command_args} -f $SCRIPT_DIR/Dockerfile.base $CERC_REPO_BASE_DIR/explorer
+docker build -t cerc/ping-pub-base:local ${build_command_args} -f $SCRIPT_DIR/Dockerfile.base $CERC_REPO_BASE_DIR/cosmos-explorer
+if [[ $? -ne 0 ]]; then
+    echo "FATAL: Base container build failed, exiting"
+    exit 1
+fi
 docker build -t cerc/ping-pub:local ${build_command_args} -f $SCRIPT_DIR/Dockerfile $SCRIPT_DIR
